2db954368343194a78f95f06ecd431cc90245fa4
SerdarOrazmyradov <mathematicaismylife@gmail.com>
2025-08-16 16:18:07 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,15 +2,25 @@ import pg from "pg";
 import "dotenv/config";
 
 export const db = new pg.Client({
-  user: process.env.DB_USER,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // user: process.env.DB_USER,
 
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
 
-  database: process.env.DB_NAME,
+  // database: process.env.DB_NAME,
 
-  password: process.env.DB_PASSWORD,
+  // password: process.env.DB_PASSWORD,
 
-  port: process.env.DB_PORT,
+  // port: process.env.DB_PORT,
 });
 
-db.connect();
+db.connect()
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
